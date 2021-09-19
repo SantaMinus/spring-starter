60f068a86d2cbf7f9581bfc2a569e4f5fc69b930
santaminus <santaminus@gmail.com>
2021-09-19 22:24:56 +0300

    Create application using the starter

---
+++ signature-service/src/main/java/com/spring/logging/signature/SignatureServiceApplication.java
@@ -0,0 +1,11 @@
+package com.spring.logging.signature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SignatureServiceApplication.class, args);
+    }
+}
