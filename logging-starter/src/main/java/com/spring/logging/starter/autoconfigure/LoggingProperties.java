f81947d96fd5b435411aa1a2d9e5a6dc59240c97
santaminus <santaminus@gmail.com>
2021-09-23 22:07:22 +0300

    Validate properties

--- logging-starter/src/main/java/com/spring/logging/starter/autoconfigure/LoggingProperties.java
+++ logging-starter/src/main/java/com/spring/logging/starter/autoconfigure/LoggingProperties.java
@@ -1,13 +1,24 @@
 package com.spring.logging.starter.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@Validated
 @ConfigurationProperties("starter.logging")
 public class LoggingProperties {
     /**
      * Message prefix for request logging
      */
+    @NotBlank
+    @Size(min = 4, max = 20)
     private String messagePrefix = "Starter > ";
+    @Valid
     private Payload payload = new Payload();
 
     public String getMessagePrefix() {
@@ -30,6 +41,8 @@ public class LoggingProperties {
         /**
          * Payload max length to be logged
          */
+        @Positive
+        @Max(100)
         private int maxLength = 100;
 
         public int getMaxLength() {
